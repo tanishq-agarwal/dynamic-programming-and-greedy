c43b4b1208ae82effab7e79b4b631766e36cd2e3
tanishq-agarwal <agarwaltanishq35@gmail.com>
2021-05-07 15:28:58 +0530

    unbounded knapsack

---
+++ unbounded knapsack/Main.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int val[] = new int[n];
+        int wt[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            val[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            wt[i] = sc.nextInt();
+        }
+        int tar = sc.nextInt();
+
+        int dp[][] = new int[n + 1][tar + 1];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (wt[i - 1] <= j) {
+                    dp[i][j] = Math.max(val[i - 1] + dp[i][j - wt[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        System.out.println(dp[n][tar]);
+    }
+}
---
+++ unbounded knapsack/README.md
@@ -0,0 +1,23 @@
+1. You are given a number n, representing the count of items.
+2. You are given n numbers, representing the values of n items.
+3. You are given n numbers, representing the weights of n items.
+3. You are given a number "cap", which is the capacity of a bag you've.
+4. You are required to calculate and print the maximum value that can be created in the bag without 
+    overflowing it's capacity.
+Note -> Each item can be taken any number of times. You are allowed to put the same item again 
+                  and again.
+
+
+# Sample Input
+
+5
+
+15 14 10 45 30
+
+2 5 1 3 4
+
+7
+
+# Sample Output
+
+100
